f644ec178255e22ac7f42360f53a87affee0198d
fermersky <forte201253@gmail.com>
2019-03-25 20:58:05 +0200

    added datamodel

--- project/project/obj/Debug/project_Content.g.i.cs
+++ project/project/obj/Debug/project_Content.g.i.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/bglogin2.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("model1.context.tt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("model1.tt")]
 
 
--- script.sql
+++ script.sql
@@ -44,12 +44,14 @@ insert into Users values ('admin', 'volodya_hlebov15') -- read and write
 create table [Certificates]
 (
 	Id int primary key identity,
-	CompanyName varchar(30),
-	[Description] varchar(30)
+	CompanyName varchar(60),
+	[Description] varchar(60)
 );
 
+
+
 insert into [Certificates] values ('html academy', 'Design web-interfaces via html и css')
-insert into [Certificates] values ('html academy', 'Programming server-side part of web-application php')
+insert into [Certificates] values ('html academy', 'Server-side part of web-application php')
 insert into [Certificates] values ('Photoshop master', 'Intermediate web-designer')
 insert into [Certificates] values ('Design for everyone', 'Awesome skills (Adobe Il)')
 insert into [Certificates] values ('CISCO', 'Professional Routing and Switching')
@@ -62,8 +64,14 @@ create table StaffandCertificates
 	CertificateId int foreign key references [Certificates](Id)
 );
 
-CREATE LOGIN Director WITH PASSWORD = '1111'
+CREATE LOGIN Director WITH PASSWORD = '123'
 USE Staff
-CREATE USER Daniil FOR LOGIN Director
+CREATE USER Daniel FOR LOGIN Director
 
-ALTER ROLE db_owner ADD MEMBER Daniil
+ALTER ROLE db_datareader ADD MEMBER Daniel
+
+CREATE LOGIN [Admin] WITH PASSWORD = '123'
+
+CREATE USER Ivan FOR LOGIN [Admin]
+
+ALTER ROLE db_owner ADD MEMBER Ivan
