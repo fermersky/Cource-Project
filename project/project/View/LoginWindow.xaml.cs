5a8d741ccbf333c7608908b50dbecde8a277d8b1
fermersky <forte201253@gmail.com>
2019-04-08 20:21:41 +0300

    search

--- project/project/View/LoginWindow.xaml.cs
+++ project/project/View/LoginWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -30,20 +31,70 @@ namespace project
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
-            var entityConn = new EntityConnectionStringBuilder(ConfigurationManager.ConnectionStrings["StaffEntities"].ConnectionString);
-            var sqlConn = new SqlConnectionStringBuilder(entityConn.ProviderConnectionString);
-
-            MessageBox.Show(sqlConn.DataSource);
-            MessageBox.Show(sqlConn.InitialCatalog);
-            MessageBox.Show(sqlConn.UserID);
-            MessageBox.Show(sqlConn.Password);
-
-            /*using (var db = new StaffEntities())
+            if (isAuthorized())
             {
                 var win = new MainWindow();
                 win.Show();
-            }*/
+            }
+        }
+
+        private bool isAuthorized()
+        {
+            var entityConn = new EntityConnectionStringBuilder(ConfigurationManager.ConnectionStrings["StaffEntities"].ConnectionString);
+            var sqlConn = new SqlConnectionStringBuilder(entityConn.ProviderConnectionString);
+            
+            if (isAreasAreFiled())
+            {
+                if (serverTb.Text == sqlConn.DataSource
+                    && databaseTb.Text == sqlConn.InitialCatalog
+                    && loginTb.Text == sqlConn.UserID
+                    && passTb.Password == sqlConn.Password)
+                {
+                    return true;
+                }
+                else
+                {
+                    ShowErrorMsg("Inputed data is wrong!");
+                    return false;
+                }
+            } 
+            else
+            {
+                ShowErrorMsg("Fill all areas!");
+                return false;
+            }
+        }
+
+        private bool isAreasAreFiled()
+        {
+            return !(string.IsNullOrEmpty(serverTb.Text)
+                || string.IsNullOrEmpty(databaseTb.Text)
+                || string.IsNullOrEmpty(loginTb.Text)
+                || string.IsNullOrEmpty(passTb.Password));
+        }
+
+        private void ShowErrorMsg(string msg)
+        {
+            var anim = new ThicknessAnimationUsingKeyFrames();
+            //anim.From = errorTb.Margin;
+            errorTb.Text = msg;
+            //anim.To = new Thickness(0, -20, 0, 0);
+            anim.KeyFrames.Add(new LinearThicknessKeyFrame(new Thickness(0, -15, 0, 0), KeyTime.FromPercent(0.50)));
+            anim.KeyFrames.Add(new LinearThicknessKeyFrame(new Thickness(0, -18, 0, 0), KeyTime.FromPercent(0.75)));
+            anim.KeyFrames.Add(new LinearThicknessKeyFrame(new Thickness(0, -20, 0, 0), KeyTime.FromPercent(1.00)));
+            anim.Duration = TimeSpan.FromSeconds(2);
+            anim.AutoReverse = true;
+            errorTb.BeginAnimation(TextBlock.MarginProperty, anim);
+        }
+
+        private void HideErrorMsg()
+        {
+            var anim = new ThicknessAnimation();
+            anim.From = errorTb.Margin;
+            anim.To = new Thickness(0, 60, 0, 0);
+            anim.BeginTime = TimeSpan.FromSeconds(3);
+            anim.Duration = TimeSpan.FromSeconds(0.3);
+            errorTb.BeginAnimation(TextBlock.MarginProperty, anim);
         }
     }
 }
--- project/project/ViewModel/ConcreteSpecViewModel.cs
+++ project/project/ViewModel/ConcreteSpecViewModel.cs
@@ -14,8 +14,95 @@ namespace project.ViewModel
 {
     public class ConcreteSpecViewModel : INotifyPropertyChanged
     {
-        private ICollectionView _workersView;
+        private string searchPattern; // textbox value
 
+        public string SearchPattern
+        {
+            get { return searchPattern; }
+            set
+            {
+                if (searchPattern != value)
+                {
+                    searchPattern = value;
+                    NotifyPropertyChanged(nameof(SearchPattern));
+                }
+            }
+        }
+
+        private bool filterBySurnameOn = false; // Surname
+        public bool FilterBySurnameOn
+        {
+            set
+            {
+                filterBySurnameOn = value;
+                NotifyPropertyChanged();
+            }
+            get => filterBySurnameOn;
+        }
+
+        private bool filterByFirstnameOn = false; // Firstname
+        public bool FilterByFirstnameOn
+        {
+            set
+            {
+                filterByFirstnameOn = value;
+                NotifyPropertyChanged();
+            }
+            get => filterByFirstnameOn;
+        }
+
+        private bool filterByLastnameOn = false; // Lastname
+        public bool FilterByLastnameOn 
+        {
+            set
+            {
+                filterByLastnameOn = value;
+                NotifyPropertyChanged();
+            }
+            get => filterByLastnameOn;
+        }
+
+        
+
+
+        private bool filterByAgeOn = false; // Age
+        public bool FilterByAgeOn
+        {
+            set
+            {
+                filterByAgeOn = value;
+                NotifyPropertyChanged();
+            }
+            get => filterByAgeOn;
+        }
+
+        private bool filterMaleOn; // is male
+
+        public bool FilterMaleOn
+        {
+            get { return filterMaleOn; }
+            set
+            {
+                filterMaleOn = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool filterFemaleOn; // is female
+
+        public bool FilterFemaleOn
+        {
+            get { return filterFemaleOn; }
+            set
+            {
+                filterFemaleOn = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+
+        private ICollectionView _workersView;
         public ICollectionView Workers
         {
             set
@@ -52,19 +139,77 @@ namespace project.ViewModel
                             .Where(w => w.FirsnName.Contains(param) && w.Specialties.SpecName == spec)
                             .ToList();
                         Workers = CollectionViewSource.GetDefaultView(filteredWorkers);
-                        Workers.Filter = CustomerFilter;
+                        Workers.Filter = CustomerFilter; // predicate
                     }
                 }));
             }
         }
 
-        private bool CustomerFilter(object item)
+        private bool CustomerFilter(object item) // predicate
         {
             var worker = item as Workers;
 
-           
+            bool result = true;
 
-            return true;
+
+            if (string.IsNullOrEmpty(SearchPattern))
+            {
+                return true;
+            }
+            else
+            {
+
+
+                if (FilterBySurnameOn)
+                {
+                    if (worker.Surname.Contains(SearchPattern))
+                        result = true & result;
+                    else
+                        result = false & result;
+                }
+                if (FilterByFirstnameOn)
+                {
+                    if (worker.FirsnName.Contains(SearchPattern))
+                        result = true & result;
+                    else
+                        result = false & result;
+                }
+                if (FilterByLastnameOn)
+                {
+                    if (worker.Lastlame.Contains(SearchPattern))
+                        result = true & result;
+                    else
+                        result = false & result;
+                }
+                if (FilterByAgeOn)
+                {
+                    /* if (worker.BirthDate == new DateTime(SearchPattern))
+                         result = true & result;
+                     else
+                         result = false & result;*/
+                    result = true & result;
+                }
+                if (FilterMaleOn)
+                {
+                    if (worker.Gender == FilterMaleOn)
+                        result = true & result;
+                    else
+                        result = false & result;
+                    result = true & result;
+                }
+                if (FilterFemaleOn)
+                {
+                    if (worker.Gender == FilterFemaleOn)
+                        result = true & result;
+                    else
+                        result = false & result;
+                    result = true & result;
+                }
+
+                return result;
+            }
+
+            
         }
 
         public string Title { get; set; } = "test";
