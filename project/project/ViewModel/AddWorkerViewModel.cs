f62b6bfd78b49da99392fde59a89f368c54ea2d0
Daniel Skrypnik <forte201253@gmail.com>
2019-04-16 14:25:02 +0300

    add worker window

--- project/project/ViewModel/AddWorkerViewModel.cs
+++ project/project/ViewModel/AddWorkerViewModel.cs
@@ -1,11 +1,15 @@
-﻿using project.Model;
+﻿using Microsoft.Win32;
+using project.Helpers;
+using project.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace project.ViewModel
 {
@@ -22,7 +26,7 @@ namespace project.ViewModel
             using (var db = new StaffEntities())
             {
                 specialties = db.Specialties.ToList();
-                
+                ImgFile = new Uri(@"../images/woman.png", UriKind.RelativeOrAbsolute);
             }
         }
 
@@ -34,6 +38,164 @@ namespace project.ViewModel
             set
             {
                 specialties = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private RelayCommand loadImageCommand;
+
+        public RelayCommand LoadImageCommand
+        {
+            get
+            {
+                return loadImageCommand ?? (loadImageCommand = new RelayCommand(obj => 
+                {
+                    var diag = new OpenFileDialog();
+                    diag.Filter = "Image Files|*.jpg;*.jpeg;*.png";
+
+                    if (diag.ShowDialog() != null)
+                    {
+                        var file = new FileInfo(diag.FileName);
+                        file.CopyTo("../../images/" + diag.SafeFileName, true);
+                       // ImgFile = diag.SafeFileName;
+                    }
+                }));
+            }
+        }
+
+        private RelayCommand addWorkerCommand;
+        public RelayCommand AddWorkerCommand
+        {
+            get
+            {
+                return addWorkerCommand ?? (addWorkerCommand = new RelayCommand((obj) => 
+                {
+                    MessageBox.Show(BirthDate.ToString());
+                }));
+            }
+        }
+
+        private Uri imgFile;
+        public Uri ImgFile
+        {
+            get => imgFile;
+            set
+            {
+                imgFile = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        //
+
+        private string firstname;
+
+        public string Firstname
+        {
+            get { return firstname; }
+            set
+            {
+                firstname = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        // 
+
+        private string lastname;
+
+        public string Lastname
+        {
+            get { return lastname; }
+            set
+            {
+                lastname = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        // 
+
+        private string surname;
+
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                surname = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        //
+
+        private bool gender = true;
+
+        public bool Gender
+        {
+            get { return gender; }
+            set
+            {
+                gender = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        //
+
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                address = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        //
+
+        private DateTime? birthDate;
+
+        public DateTime? BirthDate
+        {
+            get { return birthDate; }
+            set
+            {
+                birthDate = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        //
+
+        private int salary;
+
+        public int Salary
+        {
+            get { return salary; }
+            set
+            {
+                salary = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        //
+
+        private int specId;
+
+        public int SpecId
+        {
+            get { return specId; }
+            set
+            {
+                specId = value;
                 NotifyPropertyChanged();
             }
         }
